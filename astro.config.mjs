4cecbf580ee71b1f40cc9a7aaa0104e48fcd7284
fabiankachlock <fabian.kachlock@icloud.com>
2023-10-08 13:28:47 +0200

    feat: deploy to gh pages action

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   output: "static",
+  site: "https://fabiankachlock.github.io",
+  base: "/website_v3",
   server: {
     port: 3000,
   },
