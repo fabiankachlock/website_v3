f9cc8d832c89235ed31fa0938b2746ed3728ec80
fabiankachlock <fabian.kachlock@icloud.com>
2024-01-29 17:00:07 +0100

    feat: projects in header

--- astro-i18next.config.mjs
+++ astro-i18next.config.mjs
@@ -8,6 +8,7 @@ export default {
   routes: {
     en: {
       impressum: 'imprint',
+      projekte: 'projects',
     },
   },
 };
--- src/content/projects.ts
+++ src/content/projects.ts
@@ -3,6 +3,11 @@ import type { ProjectEntry } from './config';
 
 export type LocalizedProject = Record<string, ProjectEntry>;
 
+export type ProjectLink = {
+  title: Record<string, string>;
+  url: string;
+};
+
 const extractSlug = (slug: string) => {
   const parts = slug.split('/');
   return {
@@ -25,9 +30,32 @@ export const getPreviewProjects = async (): Promise<LocalizedProject[]> => {
       };
     }
   }
+
   return Object.values(localizedProjects).toSorted((a, b) => {
     const orderA = a[Object.keys(a)[0]!]?.data.order ?? Infinity;
     const orderB = b[Object.keys(b)[0]!]?.data.order ?? Infinity;
     return orderA - orderB;
   });
+};
+
+export const getProjectLinks = async (): Promise<ProjectLink[]> => {
+  const allProjects = await getCollection('projects');
+  const links: Record<string, ProjectLink & { order: number }> = {};
+  for (const project of allProjects) {
+    const data = extractSlug(project.slug);
+    if (!data.id || !data.lang) continue;
+    if (data.id in links) {
+      links[data.id]!.title[data.lang] = project.data.title;
+    } else {
+      links[data.id] = {
+        title: {
+          [data.lang]: project.data.title,
+        },
+        url: data.id,
+        order: project.data.order,
+      };
+    }
+  }
+
+  return Object.values(links).toSorted((a, b) => a.order - b.order);
 };
