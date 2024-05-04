251df100fdbf989dcb8aa0a6867bc54b9156be62
Fabian Kachlock <fabian.kachlock@icloud.com>
2024-05-04 18:57:40 +0200

    feat: mobile improvemnts + swiping

--- src/scripts/home.ts
+++ src/scripts/home.ts
@@ -1,12 +1,40 @@
+const content = document.querySelector('.main-content');
+
 // interactive skill drawers
-document.querySelectorAll('.skill-drawer').forEach(drawer => {
+document.querySelectorAll('.skill-drawer').forEach((drawer, idx) => {
   const id = drawer.getAttribute('data-id');
-  // document.querySelector(`[data-id="${id}"] .skill-drawer__opener`)?
-  document.querySelector(`[data-id="${id}"] .skill-drawer__head`)?.addEventListener('click', () => {
+  const head = document.querySelector(`[data-id="${id}"] .skill-drawer__head`);
+
+  head?.addEventListener('click', () => {
     drawer.classList.toggle('open');
+
+    let openDrawer: HTMLElement | null = null;
     document.querySelectorAll(`.skill-drawer:not([data-id="${id}"])`).forEach(otherDrawer => {
-      otherDrawer.classList.remove('open');
+      if (otherDrawer.classList.contains('open')) {
+        otherDrawer.classList.remove('open');
+        openDrawer = otherDrawer as HTMLElement;
+      }
     });
+
+    if (drawer.classList.contains('open') && (content?.clientWidth ?? 0) < 720) {
+      const drawerRect = drawer.getBoundingClientRect();
+      const other = openDrawer as HTMLElement | null;
+      const topSpacing = 64 + (idx === 0 ? 15 : 0);
+      const target = drawerRect.top + (content?.scrollTop ?? 0) - topSpacing;
+
+      if (other && other?.getBoundingClientRect().top < drawerRect.top) {
+        const otherHead = document.querySelector(`[data-id="${other.getAttribute('data-id')}"] .skill-drawer__head`);
+        const offset = other.getBoundingClientRect().height - (otherHead?.getBoundingClientRect()?.height || 0);
+        // console.log(otherHead?.getBoundingClientRect(), other.getBoundingClientRect());
+        content?.scrollTo({
+          top: target - offset - 31,
+        });
+      } else {
+        content?.scrollTo({
+          top: target,
+        });
+      }
+    }
   });
 });
 
--- src/scripts/setup.ts
+++ src/scripts/setup.ts
@@ -11,10 +11,11 @@ const setupTheme = () => {
     setTheme('dark');
   }
 };
+setupTheme();
 
 // toggle the menu
 const menuButtonElement = document.querySelector('.menu__opener') as HTMLButtonElement | null;
-const headElement = document.querySelector('.head');
+const headElement = document.querySelector('.head') as HTMLElement | null;
 
 if (menuButtonElement) {
   menuButtonElement.addEventListener('click', () => {
@@ -22,6 +23,14 @@ if (menuButtonElement) {
     menuButtonElement.blur();
   });
 }
+
+const tryToggleMenu = (open: boolean) => {
+  if (headElement && open && !headElement.classList.contains('open')) {
+    headElement.classList.add('open');
+  } else if (headElement && !open && headElement.classList.contains('open')) {
+    headElement.classList.remove('open');
+  }
+};
 
 // toggle language select
 document.querySelectorAll('.translate').forEach(e => {
@@ -41,4 +50,55 @@ document.querySelectorAll('#year').forEach(span => {
   span.innerHTML = new Date().getFullYear().toString();
 });
 
-setupTheme();
+// swipes
+let xStart: number | undefined = undefined;
+let xEnd: number | undefined = undefined;
+let yStart: number | undefined = undefined;
+
+const handleTouchStart = (evt: TouchEvent) => {
+  xStart = evt.touches[0]?.clientX;
+  yStart = evt.touches[0]?.clientY;
+  xEnd = undefined;
+  headElement?.classList.add('dragging');
+  document.body?.style.setProperty('--dragged-offset', '0.0001');
+};
+
+const handleTouchMove = (evt: TouchEvent) => {
+  if (!xStart || !yStart || window.innerWidth > 1024) {
+    xEnd = undefined;
+    return;
+  }
+
+  xEnd = evt.touches[0]?.clientX;
+  const minThreshold = 5;
+  const yDiff = Math.abs(yStart - (evt.touches[0]?.clientY ?? yStart));
+  const xDiff = Math.abs(xStart - (xEnd ?? xStart));
+
+  // sort ou normal scrolling and prevent menu flickering on normal scroll
+  if (yDiff > xDiff || xDiff < minThreshold) {
+    return;
+  }
+
+  // slowly reveal the menu as the user drags
+  const menuWidth = Math.min(window.innerWidth * 0.7, 300); // menuWidth = max(70vw, 300px)
+  const openThreshold = menuWidth * 0.5;
+  document.body?.style.setProperty('--dragged-offset', `${xDiff / menuWidth}`);
+
+  if (xDiff < openThreshold || !xEnd) {
+    return;
+  }
+
+  // open or close the menu after the threshold is surpassed
+  tryToggleMenu(xStart > xEnd);
+  // stop the drag
+  headElement?.classList.remove('dragging');
+  xStart = undefined;
+};
+
+const handleTouchEnd = () => {
+  headElement?.classList.remove('dragging');
+};
+
+document.addEventListener('touchstart', handleTouchStart, false);
+document.addEventListener('touchmove', handleTouchMove, false);
+document.addEventListener('touchend', handleTouchEnd, false);
