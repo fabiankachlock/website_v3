d6313d42356bb7e7c6608f156e6c5c690cbaa730
fabiankachlock <fabian.kachlock@icloud.com>
2024-05-09 18:11:35 +0200

    feat: blog image component

---
+++ src/scripts/blog.ts
@@ -0,0 +1,46 @@
+const enableImageButton = (elm: HTMLElement, isActive: boolean) => {
+  elm.setAttribute('focusable', isActive ? 'true' : 'false');
+  elm.setAttribute('tabindex', isActive ? '0' : '-1');
+  elm.setAttribute('aria-hidden', isActive ? 'false' : 'true');
+  elm.setAttribute('aria-role', isActive ? 'button' : '');
+};
+
+const menu = document.querySelector('.menu') as HTMLElement | null;
+
+document.querySelectorAll('.blog-image').forEach(image => {
+  const maximize = image.querySelector('.blog-image__action--maximize') as HTMLElement | null;
+  const minimize = image.querySelector('.blog-image__action--minimize') as HTMLElement | null;
+  const isOpen = () => image.classList.contains('blog-image--open');
+
+  const toggle = () => {
+    image.classList.toggle('blog-image--open');
+    const currentlyOpen = isOpen();
+    enableImageButton(maximize!, !currentlyOpen);
+    enableImageButton(minimize!, currentlyOpen);
+    if (!menu) return;
+
+    if (currentlyOpen) {
+      menu.style.zIndex = '0';
+    } else {
+      menu.style.zIndex = '1000';
+    }
+  };
+
+  image.querySelector('.blog-image__wrapper')?.addEventListener('click', toggle);
+  image.querySelectorAll('.blog-image__action').forEach(actionButton => {
+    actionButton?.addEventListener('keydown', e => {
+      const key = (e as KeyboardEvent).key;
+      if (key === 'Enter' || key === ' ') {
+        toggle();
+        if (isOpen()) {
+          minimize?.focus();
+        } else {
+          maximize?.focus();
+        }
+      }
+    });
+  });
+
+  enableImageButton(maximize!, true);
+  enableImageButton(minimize!, false);
+});
